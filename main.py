66fc9cf5c25518413c9f912a1f49991b4226716c
sndwichz-dev <emeraldbulandos@gmail.com>
2026-09-06 19:55:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from TikTokLive import TikTokLiveClient
 from TikTokLive.events import ConnectEvent
 
 TIKTOK_USERNAME = os.getenv("TIKTOK_USERNAME")
-DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK"),
-os.getenv("DISCORD_WEBHOOK_2")
+DISCORD_WEBHOOKS = [os.getenv("DISCORD_WEBHOOK"),
+`                 os.getenv("DISCORD_WEBHOOK_2")]
 
 client = TikTokLiveClient(unique_id=TIKTOK_USERNAME)
 
